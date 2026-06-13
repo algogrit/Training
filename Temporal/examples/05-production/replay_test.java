6e3e602d2f4008c51a0566dd44553c057635971d
Gaurav Agarwal <algogrit@gmail.com>
2026-06-13 16:46:18 +0530

    Adding Temporal as submodule.

--- Temporal/examples/05-production/replay_test.java
+++
@@ -1,9 +0,0 @@
-class ReplayHistoryTest {
-  @Test
-  void replaysProductionHistory() throws Exception {
-    WorkflowReplayer.replayWorkflowExecutionFromResource(
-        "histories/order-1001.json",
-        OrderSagaWorkflowImpl.class);
-  }
-}
-
--- Temporal/examples/runnable/01-hello-temporal/src/main/java/training/temporal/hello/GreetingActivitiesImpl.java
+++
@@ -1,9 +0,0 @@
-package training.temporal.hello;
-
-public class GreetingActivitiesImpl implements GreetingActivities {
-  @Override
-  public String composeGreeting(String name) {
-    return "Hello, " + name + " from a Temporal Activity";
-  }
-}
-
